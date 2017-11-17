fb65582d153d9c69bc56a98b56b47865af0d1d92
Héctor Delgado <hecdelflo@gmail.com>
2017-11-17 21:56:56 +0100

    solved bug runDiarization.m when outputting ala partial solutions

--- matlab/runDiarization.m
+++ matlab/runDiarization.m
@@ -133,7 +133,7 @@ if returnAllPartialSolutions
         mkdir(outputPathInd);
     end
     for i=1:k
-        getSegmentationFile(outputFormat, segmentTable, finalClusteringTable(:,i), showName{1}, strcat(showName{1}, '_', num2str(length(unique(finalClusteringTable(:,i)))), '_spk'), outputPath, outputExt);
+        getSegmentationFile(outputFormat, segmentTable, finalClusteringTable(:,i), showName{1}, strcat(showName{1}, '_', num2str(length(unique(finalClusteringTable(:,i)))), '_spk'), outputPathInd, outputExt);
     end
 end
 
